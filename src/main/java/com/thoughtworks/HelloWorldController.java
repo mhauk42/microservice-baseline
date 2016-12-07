fbfff8bde95ecb3c79dba65128d9cab61972001f
POC Demo User <poc@airplus.com>
2016-12-07 13:46:19 +0100

    HelloWorld aufgenommen

---
+++ src/main/java/com/thoughtworks/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.thoughtworks;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping(path="/")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+
+}
---
+++ src/test/java/com/thoughtworks/HelloWorldControllerTest.java
@@ -0,0 +1,19 @@
+package com.thoughtworks;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class HelloWorldControllerTest {
+
+	HelloWorldController subject= new HelloWorldController();
+	
+	@Test
+	public void shouldAnswerHello() {
+		String answer = subject.helloWorld();
+		assertThat(answer,is("Hello World"));
+		
+	}
+
+}
